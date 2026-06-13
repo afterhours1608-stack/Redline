a5d6587ab1a8cb2a875c11f67c8afb621916b9dc
transkpgg <transkpgg@gmail.com>
2026-06-13 11:35:47 +0700

    Fix port and proxy configuration

--- vite.config.js
+++ vite.config.js
@@ -24,10 +24,14 @@ export default defineConfig({
     },
   },
   server: {
-    port: 5173,
+    port: 3000,
     open: true,
     proxy: {
       '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      },
+      '/uploads': {
         target: 'http://localhost:5000',
         changeOrigin: true
       }
