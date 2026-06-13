5636d73b4caa07fd949ff2bf7fdb4ba44b7652c7
transkpgg <transkpgg@gmail.com>
2026-06-13 12:18:31 +0700

    Test Vercel initialization

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import ordersRoutes from './routes/orders.js';
 import dashboardRoutes from './routes/dashboard.js';
 import usersRoutes from './routes/users.js';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 const app = express();
 
 app.use(cors());
