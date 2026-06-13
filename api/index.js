8986c870b25852c2d80ebe832f12bfaf69f20aeb
transkpgg <transkpgg@gmail.com>
2026-06-13 12:13:41 +0700

    Debug Vercel API Crash

--- api/index.js
+++ api/index.js
@@ -1,2 +1,9 @@
-import app from '../server/server.js';
-export default app;
+export default async (req, res) => {
+  try {
+    const { default: app } = await import('../server/server.js');
+    return app(req, res);
+  } catch (err) {
+    console.error("VERCEL INIT ERROR:", err);
+    res.status(500).json({ error: "Init Error", message: err.message, stack: err.stack });
+  }
+}
